955842e743de05ef486512f78a0f8ca04a5f8fca
Vaibhav Chavhan <chavhanvaibhav708@gmail.com>
2026-05-22 11:00:39 +0000

    add db engine and session

---
+++ python-backend/db/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from config import DATABASE_URL
+
+engine = create_engine(
+    DATABASE_URL,
+)
+
+SessionLocal = sessionmaker(
+    bind = engine,
+    autocommit = False,
+    autoflush = False
+)
+
+Base = declarative_base()
